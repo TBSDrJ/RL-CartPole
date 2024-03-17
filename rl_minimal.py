c1867a95ee9ccebb5fcc977212b3b718d1aef5f8
TBSDrJ <marcus.jaiclin@bishops.com>
2024-03-17 15:25:16 -0700

    Add the network and agent.

--- rl_minimal.py
+++ rl_minimal.py
@@ -7,7 +7,23 @@ https://www.tensorflow.org/agents/tutorials/5_replay_buffers_tutorial#using_repl
 
 from tf_agents.environments import suite_gym
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
+from tf_agents.networks.q_network import QNetwork
+from tf_agents.agents import DqnAgent
+import tensorflow.keras.optimizers.legacy as optimizers
 
 # 1. Environment
 py_env = suite_gym.load('CartPole-v0')
 tf_env = TFPyEnvironment(py_env)
+
+# 2. Network/Model/Agent
+model = QNetwork(
+    tf_env.time_step_spec().observation,
+    tf_env.action_spec(),
+    fc_layer_params = (100, 50), # This is what is used in the larger Deep Q ex
+)
+agent = DqnAgent(
+    tf_env.time_step_spec(),
+    tf_env.action_spec(),
+    q_network = model,
+    optimizer = optimizers.Adam(learning_rate = 0.001)
+)
