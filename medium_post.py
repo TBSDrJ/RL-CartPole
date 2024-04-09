4238d12536af67c08fdd592d2d3bfd392f600cd7
TBSDrJ <marcus.jaiclin@bishops.com>
2024-04-09 09:58:26 -0700

    A few reorganizations of medium_post.py

--- medium_post.py
+++ medium_post.py
@@ -14,6 +14,34 @@ from tf_agents.policies import random_tf_policy
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents import trajectories
 
+# utility function to compute average return
+def compute_avg_return(environment, policy, num_episodes=10):
+    total_return = 0.0
+    for _ in range(num_episodes):
+        time_step = environment.reset()
+        episode_return = 0.0
+        
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+        total_return += episode_return
+        
+    avg_return = total_return / num_episodes
+    return avg_return.numpy()[0]
+
+# functions to collect data to the replay buffer
+def collect_step(environment, policy, buffer):
+    time_step = environment.current_time_step()
+    action_step = policy.action(time_step)
+    next_time_step = environment.step(action_step.action)
+    traj = trajectories.from_transition(time_step, action_step, next_time_step)
+    buffer.add_batch(traj)
+    
+def collect_data(env, policy, buffer, steps):
+    for i in range(steps):
+        collect_step(env, policy, buffer)
+
 # Supress Warnings
 tf.get_logger().setLevel('ERROR')
 
@@ -23,42 +51,11 @@ replay_buffer_max_length = 100000
 batch_size = 64
 learning_rate = 1e-3
 log_interval = 200
-num_eval_episodes = 10
+num_eval_episodes = 20
 eval_interval = 1000
 
 # variable to store name of the environment
 env_name = 'CartPole-v0'
-
-# loading the environment
-# env = suite_gym.load(env_name)
-
-# create an empty dataframe
-# df = pd.DataFrame(columns=['step_type', 'reward', 'discount', 'observation'])
-
-# initial state
-# time_step = env.reset()
-# df = df._append(
-#     {
-#     'step_type': time_step.step_type, 
-#     'reward': time_step.reward, 
-#     'discount': time_step.discount, 
-#     'observation': time_step.observation
-#     }, 
-#     ignore_index=True
-# )
-
-# iterate while the time_step is not the last
-# while not time_step.is_last():
-#     time_step = env.step(np.array([1]))
-#     df = df._append(
-#         {
-#         'step_type': time_step.step_type, 
-#         'reward': time_step.reward, 
-#         'discount': time_step.discount, 
-#         'observation': time_step.observation
-#         }, 
-#         ignore_index=True
-#     )
 
 # two environments, one for training and one for evaluation
 train_py_env = suite_gym.load(env_name)
@@ -103,42 +100,12 @@ collect_policy = agent.collect_policy
 random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
                                                 train_env.action_spec())
 
-# utility function to compute average return
-def compute_avg_return(environment, policy, num_episodes=10):
-    total_return = 0.0
-    for _ in range(num_episodes):
-        time_step = environment.reset()
-        episode_return = 0.0
-        
-        while not time_step.is_last():
-            action_step = policy.action(time_step)
-            time_step = environment.step(action_step.action)
-            episode_return += time_step.reward
-        total_return += episode_return
-        
-    avg_return = total_return / num_episodes
-    return avg_return.numpy()[0]
-
 # replay buffer to collect the trajectories
 replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
     data_spec=agent.collect_data_spec,
     batch_size=train_env.batch_size,
     max_length=replay_buffer_max_length
 )
-
-# functions to collect data to the replay buffer
-def collect_step(environment, policy, buffer):
-    time_step = environment.current_time_step()
-    action_step = policy.action(time_step)
-    next_time_step = environment.step(action_step.action)
-    traj = trajectories.from_transition(time_step, action_step, next_time_step)
-    print(traj)
-    buffer.add_batch(traj)
-    
-def collect_data(env, policy, buffer, steps):
-    for i in range(steps):
-        collect_step(env, policy, buffer)
-        print(i)
         
 collect_data(train_env, random_policy, replay_buffer, steps=100)
 
@@ -171,8 +138,6 @@ for i in range(num_iterations):
     for _ in range(collect_steps_per_iteration):
         collect_step(train_env, agent.collect_policy, replay_buffer)
 
-    print(i)
-        
     # take a batch of data from the replay buffer and train the network
     experience, unused_info = next(iterator)
     train_loss = agent.train(experience).loss
